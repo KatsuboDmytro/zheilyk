b50f0eb32960e224c1a6b2042f2876183c628024
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-07-26 19:51:48 +0300

    made an approximate view of the footer

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './footer.scss';
 
-export const Footer:React.FC = () => {
+export const Footer: React.FC = () => {
   return (
-    <div>Footer.component</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer__box container">
+        <Link to="/">
+          <img
+            src="img/icons/main-logo.svg"
+            alt="Zheilyk Multibrand store"
+            className="header__logo"
+          />
+        </Link>
+        <div className="footer__info">
+          <button className="footer__language">
+            <img
+              src="img/icons/language.svg"
+              alt="world icon"
+              className="footer__language--icon"
+            />
+            <span className="footer__language--text">Українська</span>
+            <img
+              src="img/icons/chevron (arrow down).svg"
+              alt="chevron"
+              className="footer__language--icon"
+            />
+          </button>
+          <a
+            href="https://www.instagram.com/multibrand_zheilyk?igsh=MzRoOTJrM3VybjFk"
+            className="footer__social"
+          >
+            <img
+              src="img/icons/instagram.svg"
+              alt="instagram icon"
+              className="footer__socials--icon"
+            />
+            <span>Instagram</span>
+          </a>
+          <ul className="footer__useful">
+            <li className="footer__useful--item">
+              <Link to="/about" className="footer__useful--link">
+                Контакти
+              </Link>
+            </li>
+            <li className="footer__useful--item">
+              <Link to="/contacts" className="footer__useful--link">
+                Обмін і повернення
+              </Link>
+            </li>
+            <li className="footer__useful--item">
+              <Link to="/delivery" className="footer__useful--link">
+                FAQs
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
---
+++ src/modules/LogIn/LogIn.module.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LogIn:React.FC = () => {
+  return (
+    <div>LogIn:React.FC</div>
+  )
+}
